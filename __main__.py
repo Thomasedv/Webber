b49c7b48b318c9406589709af4e1671ec7db7ca6
Thomasedv <thomas.edvardsen@hotmail.com>
2019-09-28 23:13:18 +0900

    Fixed working directory check.

--- __main__.py
+++ __main__.py
@@ -6,7 +6,9 @@ import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
 
+cwd_was_sys32 = False
 if os.getcwd().lower() == r'c:\windows\system32'.lower():  # Bit of a hack, but if you have this, your fault
+    cwd_was_sys32 = True
     # Check if running as script, or executable.
     if getattr(sys, 'frozen', False):
         application_path = os.path.dirname(sys.executable)
@@ -20,23 +22,6 @@ from utils import get_logger
 
 def main():
     log = get_logger('Webber')
-    log.info('Webber has been started...')
-
-    log.info('Checking working directory...')
-
-    if os.getcwd().lower() == r'c:\windows\system32'.lower():  # Bit of a hack, but if you have this, your fault
-        log.info('Working directory is system32!')
-
-        # Check if running as script, or executable.
-        if getattr(sys, 'frozen', False):
-            application_path = os.path.dirname(sys.executable)
-        else:
-            application_path = os.path.dirname(__file__)
-        os.chdir(os.path.realpath(application_path))
-
-        log.info(f'Working directory is set to {application_path}')
-    else:
-        log.info('Working directory is ok')
 
     app = QApplication(sys.argv)
     log.info('Starting GUI...')
@@ -50,6 +35,10 @@ def main():
 
 
 if __name__ == '__main__':
-    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
-    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+    log = get_logger('Webber')
+    log.info('Webber has been started...')
+
+    if cwd_was_sys32:
+        log.warning(f'Working dir was system32, changed to {os.getcwd().lower()}')
+
     main()
